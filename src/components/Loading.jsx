655072e247c73b5c130132cd00ac33ad724674cb
OmkarAwhad <omkarawhad3007@gmail.com>
2025-08-13 20:04:39 +0530

    some changes

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from 'react';
 
 function Loading() {
   return (
-    <div className='w-full h-screen flex items-center bg-black justify-center'>
-      <img src='/gif/loader.gif' alt="" className='h-[50%] ' />
+    <div className="w-full h-screen flex items-center justify-center" style={{ backgroundColor: '#1F1E24' }}>
+      <div className="spinner"></div>
+      <style jsx>{`
+        .spinner {
+          width: 80px;
+          height: 80px;
+          position: relative;
+        }
+
+        .spinner::before,
+        .spinner::after {
+          content: '';
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          border: 6px solid transparent;
+          border-top-color: #ffffff;
+          border-radius: 50%;
+          animation: spin 0.6s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+        }
+
+        .spinner::after {
+          border: 6px solid transparent;
+          border-bottom-color: #ffffff;
+          animation-direction: reverse;
+          animation-duration: 0.8s;
+        }
+
+        @keyframes spin {
+          0% { transform: rotate(0deg); }
+          100% { transform: rotate(360deg); }
+        }
+
+        .spinner::before {
+          box-shadow: 0 0 10px rgba(255, 255, 255, 0.3);
+        }
+      `}</style>
     </div>
-  )
+  );
 }
 
-export default Loading
+export default Loading;
--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const instance = axios.create({
-     baseURL:"https://api.themoviedb.org/3/",
-     headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZGUzOGFkNTBiYTZmMDllN2RkYjBiODU4ZmViNzUwMiIsIm5iZiI6MTcxOTY1MjE5OC44NzEwMjksInN1YiI6IjY2N2ZjZDNhNTQyZjgyNzI4ZWFjMTc2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kV0vAl7WYmOY2Twy9hfpR9PKamXMbkhUKp93c2jg-GA'
-        }
+	baseURL: "https://api.themoviedb.org/3/",
+	headers: {
+		accept: "application/json",
+		Authorization:
+			"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YTA1NGY5M2Y2NTc1ZGZiZDBjMDM4MzU2ZDE3NTNmZCIsIm5iZiI6MTc1NTA5NDUwNi42MjMwMDAxLCJzdWIiOiI2ODljOWRlYTNkOGQ2MWUxOGQyNjhkZTkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.DjZh_BgWgeJNznw4VHFsdk61khcZNbK87mO44wDkLt8",
+	},
 });
 
-export default instance
+export default instance;
